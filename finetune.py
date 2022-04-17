6cf59559b59fd49c31943c485d12ebc81c459352
Nuri Kim <nuri.kim@rllab.snu.ac.kr>
2022-04-17 10:42:19 +0900

    add test code

--- finetune.py
+++ finetune.py
@@ -58,8 +58,8 @@ cfg.DATALOADER.NUM_WORKERS = 4
 cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")  # Let training initialize from model zoo
 cfg.SOLVER.IMS_PER_BATCH = 32
 cfg.SOLVER.BASE_LR = 0.0005  # pick a good LR
-cfg.SOLVER.MAX_ITER = 180000
-cfg.SOLVER.STEPS = [80000, 120000, 150000]        # do not decay learning rate
+cfg.SOLVER.MAX_ITER = 120000
+cfg.SOLVER.STEPS = [80000, 100000]        # do not decay learning rate
 cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 512   # faster, and good enough for this toy dataset (default: 512)
 cfg.OUTPUT = os.path.join(cfg.OUTPUT, args.dataset)
   # only has one class (ballon). (see https://detectron2.readthedocs.io/tutorials/datasets.html#update-the-config-for-new-datasets)
