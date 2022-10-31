f3af0df78d471a1be34d300bee6e0af1c6c36528
Nuri Kim <nuri.kim@rllab.snu.ac.kr>
2022-10-31 21:54:51 +0900

    add finetuning code

--- finetune_directional.py
+++ finetune_directional.py
@@ -8,7 +8,13 @@ import os, sys
 os.environ["PYTHONUNBUFFERED"] = "1"
 import detectron2 as detectron2_ # importing the installed module
 
-sys.path.insert(0, '.')
+# sys.path.insert(0, '.')
+sys.path.insert(0, '/home/blackfoot/codes/detectron2D')
+# sys.path.insert(0, '/home/blackfoot/codes/detectron2D/tools')
+# sys.path.insert(0, '/home/blackfoot/codes/detectron2D/detectron2')
+# sys.path.insert(0, '/home/blackfoot/codes')
+del sys.modules["detectron2"]
+# sys.path.insert(0, '.')
 # sys.path.insert(0, '/home/blackfoot/codes/detectron2_')
 # del sys.modules["detectron2"]
 # sys.path.insert(0, '/home/blackfoot/codes/detectron2_/detectron2')
