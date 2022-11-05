0a432ce6874a69dda069255c83becf076f0ec8cf
Nuri Kim <nuri.kim@rllab.snu.ac.kr>
2022-11-05 16:22:53 +0900

    add finetuning code

--- tools/train_net.py
+++ tools/train_net.py
@@ -125,6 +125,19 @@ def setup(args):
 
 def main(args):
     cfg = setup(args)
+    # test_dataset_dicts = load_coco_json(os.path.join(f"/home/blackfoot/codes/Object-Graph-Memory/data/{args.dataset}_{args.tag}/instances_{args.split}.json"),
+    #                                     image_root=f"/home/blackfoot/codes/Object-Graph-Memory/data/{args.dataset}_{args.tag}/{args.split}",
+    #                                     dataset_name=f"{args.dataset}_{args.tag}_{args.split}", extra_annotation_keys=None)
+    json_file = os.path.join(f"/home/blackfoot/codes/Object-Graph-Memory/data/{args.dataset}_{args.tag}/instances_{args.split}.json")
+    with open(json_file) as f:
+        imgs_data = json.load(f)
+    categories = [imgs_data['categories'][i]['name'] for i in range(len(imgs_data['categories']))]
+    del imgs_data
+    register_coco_instances(f"{args.dataset}_{args.tag}_train", {},
+                            f"/home/blackfoot/codes/Object-Graph-Memory/data/{args.dataset}_{args.tag}/instances_train.json",
+                            f"/home/blackfoot/codes/Object-Graph-Memory/data/{args.dataset}_{args.tag}/train")
+    train_dataset_metadata = MetadataCatalog.get(f"{args.dataset}_{args.tag}_train")
+    train_dataset_metadata.set(thing_classes=categories)
 
     if args.eval_only:
         model = Trainer.build_model(cfg)
@@ -158,19 +171,7 @@ if __name__ == "__main__":
     args.tag = "withoutseg"
     args.split = "val"
     print("Command Line Args:", args)
-    test_dataset_dicts = load_coco_json(os.path.join(f"/home/blackfoot/codes/Object-Graph-Memory/data/{args.dataset}_{args.tag}/instances_{args.split}.json"),
-                                        image_root=f"/home/blackfoot/codes/Object-Graph-Memory/data/{args.dataset}_{args.tag}/{args.split}",
-                                        dataset_name=f"{args.dataset}_{args.tag}_{args.split}", extra_annotation_keys=None)
-    json_file = os.path.join(f"/home/blackfoot/codes/Object-Graph-Memory/data/{args.dataset}_{args.tag}/instances_{args.split}.json")
-    with open(json_file) as f:
-        imgs_data = json.load(f)
-    categories = [imgs_data['categories'][i]['name'] for i in range(len(imgs_data['categories']))]
-    del imgs_data
-    register_coco_instances(f"{args.dataset}_{args.tag}_train", {},
-                            f"/home/blackfoot/codes/Object-Graph-Memory/data/{args.dataset}_{args.tag}/instances_train.json",
-                            f"/home/blackfoot/codes/Object-Graph-Memory/data/{args.dataset}_{args.tag}/train")
-    train_dataset_metadata = MetadataCatalog.get(f"{args.dataset}_{args.tag}_train")
-    train_dataset_metadata.set(thing_classes=categories)
+
     launch(
         main,
         args.num_gpus,
